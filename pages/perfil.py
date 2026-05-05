d9e53dea6b22a780280e9a60073ce2c43cd6f371
Trailrunner1982 <aalmeidalopes@gmail.com>
2026-05-05 20:57:19 +0100

    Update perfil.py

--- pages/perfil.py
+++ pages/perfil.py
@@ -1,31 +1,25 @@
 import streamlit as st
 from database import get_conn
 
+
 def perfil_page(user_id):
-    st.header("Perfil")
+    st.header("Perfil Pessoal")
 
-    fc_max = st.number_input("FC Max")
-    fc_rep = st.number_input("FC Repouso")
-    dias = st.multiselect("Dias treino", list(range(7)))
-    longo = st.selectbox("Dia longo", list(range(7)))
-    volume = st.number_input("Horas semana")
+    conn = get_conn()
+    c = conn.cursor()
 
-    if st.button("Guardar"):
-        conn = get_conn()
-        c = conn.cursor()
+    c.execute("SELECT * FROM perfil WHERE user_id=?", (user_id,))
+    p = c.fetchone()
 
+    nome = st.text_input("Nome", value=p[1] if p else "")
+    data = st.date_input("Data de nascimento")
+    altura = st.number_input("Altura (cm)", value=p[3] if p else 170)
+    peso = st.number_input("Peso (kg)", value=p[4] if p else 70)
+
+    if st.button("Guardar perfil"):
         c.execute("""
-        UPDATE users
-        SET fc_max=?, fc_repouso=?, dias_treino=?, dia_longo=?, volume_horas=?
-        WHERE id=?
-        """, (
-            fc_max,
-            fc_rep,
-            ",".join(map(str, dias)),
-            longo,
-            volume,
-            user_id
-        ))
-
+        INSERT OR REPLACE INTO perfil (user_id, nome, data_nascimento, altura, peso)
+        VALUES (?, ?, ?, ?, ?)
+        """, (user_id, nome, str(data), altura, peso))
         conn.commit()
-        st.success("Guardado")
+        st.success("Perfil guardado")
