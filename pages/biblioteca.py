24e3ea2fd133776659bc381719b5a62bc5d24697
Trailrunner1982 <aalmeidalopes@gmail.com>
2026-05-05 10:17:31 +0100

    Update biblioteca.py

--- pages/biblioteca.py
+++ pages/biblioteca.py
@@ -1,1 +1,24 @@
+import streamlit as st
+from database import get_conn
 
+
+def biblioteca_page():
+    st.header("Biblioteca")
+
+    conn = get_conn()
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM biblioteca")
+    items = c.fetchall()
+
+    for i in items:
+        st.subheader(i[1])
+        st.write(f"Tipo: {i[2]}")
+        st.write(i[4])
+
+        if i[2] == "video":
+            st.video(i[3])
+        else:
+            st.markdown(f"[Abrir artigo]({i[3]})")
+
+        st.write("---")
