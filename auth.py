51ca31b110dd1e5b391a4370da1d1679ad365af1
Trailrunner1982 <aalmeidalopes@gmail.com>
2026-05-05 09:27:36 +0100

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,23 @@
+import streamlit as st
+from database import get_conn
 
+def login():
+    st.title("Login")
+
+    user = st.text_input("Username")
+    pw = st.text_input("Password", type="password")
+
+    if st.button("Entrar"):
+        conn = get_conn()
+        c = conn.cursor()
+
+        c.execute("SELECT * FROM users WHERE username=? AND password=?", (user, pw))
+        res = c.fetchone()
+
+        if res:
+            st.session_state["user_id"] = res[0]
+            st.session_state["role"] = res[3]
+            st.success("Login OK")
+            st.rerun()
+        else:
+            st.error("Credenciais inválidas")
