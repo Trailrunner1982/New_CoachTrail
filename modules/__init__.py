f0337366882305a869d6e3e77f01b375b77611d3
Trailrunner1982 <aalmeidalopes@gmail.com>
2026-05-06 21:41:29 +0100

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,7 +1,1 @@
-# criar admin automaticamente
-c.execute("SELECT * FROM users WHERE username='admin'")
-if not c.fetchone():
-    c.execute("""
-    INSERT INTO users (username, password, role)
-    VALUES ('admin', 'admin123', 'admin')
-    """)
+
