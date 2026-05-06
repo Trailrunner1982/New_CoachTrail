5ecc9d974111c89ff5e4e2bb338a7b2dc3ea6841
Trailrunner1982 <aalmeidalopes@gmail.com>
2026-05-06 15:01:53 +0100

    Update treino_engine.py

--- treino_engine.py
+++ treino_engine.py
@@ -4,26 +4,39 @@ from datetime import timedelta
 def gerar_plano(data_inicio, data_fim):
     plano = []
     d = data_inicio
+    semana = 1
 
     while d <= data_fim:
 
+        tipo = "Z2"
+        desc = f"""
+🏃 Endurance
+
+🔹 8-12 km
+🔹 60 min
+🔹 Zona 2
+
+🎯 Base aeróbica
+"""
+
+        if semana % 2 == 0:
+            tipo = "Intervalado"
+            desc = """
+🏃 Intervalado
+
+🔹 10km total
+🔹 5x(3min Z4 + 2min Z1)
+
+🎯 VO2max
+"""
+
         plano.append({
             "data": d.strftime("%Y-%m-%d"),
-            "tipo": "Z2",
-            "descricao": f"""
-🏃 Treino Endurance
-
-Distância: 8-12km
-Duração: 50-70 min
-Zona: Z2
-
-🎯 Objetivo:
-Desenvolver base aeróbica
-
-⚠ Manter esforço confortável
-"""
+            "tipo": tipo,
+            "descricao": desc
         })
 
         d += timedelta(days=1)
+        semana += 1
 
     return plano
